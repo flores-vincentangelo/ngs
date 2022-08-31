4f8e66bc688351cec9526ac9ffd3d7d9b9d89dfc
Vincent Flores <94419368+flores-vincentangelo@users.noreply.github.com>
2022-08-31 16:08:00 +0800

    updates

--- 5-web/2-express/index.js
+++ 5-web/2-express/index.js
@@ -2,6 +2,14 @@ const express = require('express');
 
 const server = express();
 
+server.get('/', (req, res) => {
+  res.send("hello express");
+})
+
+server.get('/test', (req, res) => {
+  res.send("test");
+})
+
 server.listen(4242, () => {
   console.log('Express Server is running...');
 });
